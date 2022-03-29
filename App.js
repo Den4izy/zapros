8964bc194ecd131d1ac57de34c4b93827ca8a6a0
Denys Demydenko <kawoonne@gmail.com>
2022-03-29 08:36:54 +0300

    End

--- App.js
+++ App.js
@@ -1,6 +1,47 @@
 const http = require('http');
+const url = require('url');
+const { parse } = require('querystring');
 
 http.createServer((request, response)=>{
     console.log('Server Work');
-    response.end('This work!');
-}).listen(3001);
+    console.log('________________________________');
+    // метод GET
+    // параметри запроса
+    if(request.method=='GET'){
+        console.log(request);
+        console.log('________________________________');
+        // параметри конкретних даних
+        console.log(request.method);
+        console.log('________________________________');
+        // розбираємо url запроса на параметри
+        let urlRequest = url.parse(request.url, true);
+        console.log(urlRequest);
+        console.log('________________________________');
+        // отртимуємо конкретний параметр url
+        console.log(urlRequest.query.test);
+        console.log('________________________________');
+        // отримуємо на сервер параметр, опрацьовуємо його і відправляємо назад і виводимо в браузер
+        response.end('Привіт ' + urlRequest.query.test);
+    }
+    else{
+        // метод POST
+        let body = '';
+        request.on('data', chunk =>{
+            // записуємо інформація частинами по чанкам
+            body += chunk.toString();
+        });
+        request.on('end', ()=>{
+            // виводимо тіло запроса( получається query строка)
+            console.log(body);
+            // розбиваємо строку на параметри( получаємо асоціативний масів)
+            let params = parse(body);
+            console.log(params);
+            //отримуємо конкретний параметр
+            let name = params.name;
+            console.log(params.name);
+            // робимо відповідь від сервера( обробляємо і надсилаємо наш параметр)
+            response.end('Привіт ' + name);
+        })
+    }
+
+}).listen(9999);
